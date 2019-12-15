0d60d3fbdc8799dfeb06e333220d49cf6cf85f3c
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-15 03:03:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] = merge "Assembly" 
+  programmer_hash[:dennis_ritchie][:languages] = .merge "Assembly" 
 end
